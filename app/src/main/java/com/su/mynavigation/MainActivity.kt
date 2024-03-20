9a725d52cbffc911278ea01dba5230c3bff427a7
Ruihao Zhang <91977448+RuihaoZhang123@users.noreply.github.com>
2024-03-20 12:29:39 +0800

    Add files via upload

--- app/src/main/java/com/su/mynavigation/MainActivity.kt
+++ app/src/main/java/com/su/mynavigation/MainActivity.kt
@@ -3,8 +3,14 @@ package com.su.mynavigation
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.InputType
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.EditText
+import android.widget.ImageButton
 import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.material.bottomnavigation.BottomNavigationView
@@ -14,11 +20,16 @@ class MainActivity : AppCompatActivity() {
     private lateinit var bottomNav : BottomNavigationView
     private lateinit var db : DatabaseHelper
     private lateinit var userList : List<UserListModel>
+    private lateinit var dashboard: Dashboard
+    private lateinit var dashboardView: View
+    private lateinit var mainActivityInstance: MainActivity
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.fragment_dashboard)
-
+        dashboardView = window.decorView.rootView
+        dashboard = Dashboard()
+        mainActivityInstance = this
         db = DatabaseHelper(this)
         //db.deleteAll()
         //val user1 = UserListModel( "Nathan", "OMG HELLO! Howdy There! My name is John Doe! I am a thrilled user of Guacagoalie who loves travelling, having fun, and working out! I see myself using this app to help make my future more active and pleasant.\"\n", 0, 0, 0, 0, 0)
@@ -30,6 +41,8 @@ class MainActivity : AppCompatActivity() {
             finish()
         }
         else{
+
+
             val name = userList[0].name
             val about = userList[0].about
             val goal = userList[0].goal
@@ -39,10 +52,12 @@ class MainActivity : AppCompatActivity() {
             dName.text = name
             dAbout.text = about
             dGoal.text = goal.toString()
+
+            
         }
 
-
-
+        val milestoneButton = findViewById<ImageButton>(R.id.miltstoneGoalButton)
+        val imageButton = findViewById<ImageButton>(R.id.stepsTodayButton3)
 
         bottomNav = findViewById<BottomNavigationView>(R.id.bottomNav)
         bottomNav.selectedItemId = R.id.dashboard
@@ -50,8 +65,10 @@ class MainActivity : AppCompatActivity() {
         bottomNav.setOnItemSelectedListener {
             when (it.itemId) {
                 R.id.dashboard -> {
+
                 }
                 R.id.rewards -> {
+
                 }
                 R.id.map -> {
                     val nextPage = Intent(this, Map::class.java)
@@ -61,9 +78,18 @@ class MainActivity : AppCompatActivity() {
             }
             true
         }
+
+
+        milestoneButton.setOnClickListener{
+            dashboard.accessMilestone(mainActivityInstance)
+        }
+
+        imageButton.setOnClickListener {
+            dashboard.acessDaily(dashboardView, mainActivityInstance)
+        }
+
+
     }
-
-
 
 
 }
